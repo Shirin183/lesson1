584379b5ba1797034881c75bfc2c2254179eb7b2
Shirin183 <rumayarumiya@gmail.com>
2025-03-13 10:18:46 +0500

    Add files via upload

---
+++ lesson1/script.js
@@ -0,0 +1,14 @@
+let name = prompt("ismingizi kiriting")
+console.log(name);
+
+let familiya = prompt("Familiyanizni kiriting")
+console.log(familiya)
+
+let yosh = prompt("Yoshingizni kiriting")
+console.log(yosh)
+
+let fan = prompt("Yoqtirgan faningizni yozing")
+console.log(fan)
+
+let tuman = prompt("Yashsydigan tumangizni kiriting")
+console.log(tuman)
